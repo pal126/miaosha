f98323ecd7e9942d1df021e9ec973e8d582b985e
pal126 <wangkang@jskerun.com.cn>
2018-05-23 11:27:50 +0800

    rabbitMQ exchange

--- src/main/java/com/pal/miaosha/rabbitmq/MQReceiver.java
+++ src/main/java/com/pal/miaosha/rabbitmq/MQReceiver.java
@@ -1,14 +1,36 @@
 package com.pal.miaosha.rabbitmq;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+/**
+ * rabbitMQ Receiver
+ * @author pal
+ * @date 2018/05/21
+ */
+@Slf4j
 @Service
 public class MQReceiver {
 
     @RabbitListener(queues = MQConfig.QUEUE)
     public void receice(String message) {
+        log.info("msg:{}",message);
+    }
 
+    @RabbitListener(queues = MQConfig.TOPIC_QUEUE1)
+    public void receiceTopic1(String message) {
+        log.info("msg:{}",message);
+    }
+
+    @RabbitListener(queues = MQConfig.TOPIC_QUEUE2)
+    public void receiceTopic2(String message) {
+        log.info("msg:{}",message);
+    }
+
+    @RabbitListener(queues = MQConfig.HEADERS_QUEUE)
+    public void receiceHeader(byte[] message) {
+        log.info("msg:{}",message.toString());
     }
 
 }
--- src/main/java/com/pal/miaosha/rabbitmq/MQSender.java
+++ src/main/java/com/pal/miaosha/rabbitmq/MQSender.java
@@ -2,17 +2,60 @@ package com.pal.miaosha.rabbitmq;
 
 import com.pal.miaosha.redis.RedisService;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * rabbitMQ Sender
+ * @author pal
+ * @date 2018/05/21
+ */
 @Service
 public class MQSender {
 
     @Autowired
     AmqpTemplate amqpTemplate;
 
+    /**
+     * Direct
+     * @param message
+     */
     public void send(Object message) {
         String msg = RedisService.beanToString(message);
         amqpTemplate.convertAndSend(MQConfig.QUEUE,msg);
+    }
+
+    /**
+     * Topic
+     * @param message
+     */
+    public void sendTopic(Object message) {
+        String msg = RedisService.beanToString(message);
+        amqpTemplate.convertAndSend(MQConfig.TOPIC_EXCHANGE, MQConfig.ROUTING_KEY1, msg + "1");
+        amqpTemplate.convertAndSend(MQConfig.TOPIC_EXCHANGE, MQConfig.ROUTING_KEY2, msg + "2");
+    }
+
+    /**
+     * Fanout
+     * @param message
+     */
+    public void sendFanout(Object message) {
+        String msg = RedisService.beanToString(message);
+        amqpTemplate.convertAndSend(MQConfig.FANOUT_EXCHANGE, msg);
+    }
+
+    /**
+     * Header
+     * @param message
+     */
+    public void sendHeader(Object message) {
+        String msg = RedisService.beanToString(message);
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.setHeader("header1", "value1");
+        messageProperties.setHeader("header2", "value2");
+        Message obj = new Message(msg.getBytes(), messageProperties);
+        amqpTemplate.convertAndSend(MQConfig.HEADERS_EXCHANGE, "", obj);
     }
 }
